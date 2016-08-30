93d54ed8c924f8f57a7cd3328cbd6ea4b480f857
Rick Wilde <rkwilde@gmail.com>
2016-08-30 16:04:10 -0600

    updated landing views with database tables set up

--- estate_database.sql
+++ estate_database.sql
@@ -1,3 +1,5 @@
+/*PHP updates make 0000-00-00 0:00:00 not a valid default for datetimes. Changed these to NULL defaults*/
+
 CREATE TABLE IF NOT EXISTS `person` (
   `person_id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(100) NOT NULL DEFAULT '',
@@ -30,17 +32,17 @@ CREATE TABLE IF NOT EXISTS `estate` (
   `estate_name` varchar(100) NOT NULL DEFAULT '',
   `estate_status_id` int(11) NOT NULL DEFAULT '0', /* 1=open, 2=settled */
   `payment_amount_due` decimal(10, 2) NOT NULL DEFAULT 0,
-  `payment_due_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `payment_due_date` datetime DEFAULT NULL,
   `customer_type_id` int(11) NOT NULL DEFAULT '0', /* 1=paid, 2=trial, 3=free */
-  `trial_start_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `trial_end_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `trial_start_date` datetime DEFAULT NULL,
+  `trial_end_date` datetime DEFAULT NULL,
   `allow_outside_money` tinyint(2) NOT NULL DEFAULT 0, /* let rich brother buy more */
   `sealed_bids` tinyint(2) NOT NULL DEFAULT 0, /* sealed or public bidding */
   `initial_cash` decimal(12, 2) NOT NULL DEFAULT 0, /* cash accounts of estate */
   `liquidated_cash` decimal(12, 2) NOT NULL DEFAULT 0, /* value of sold items */
   `test` tinyint(2) NOT NULL DEFAULT '0',
   `active` tinyint(2) NOT NULL DEFAULT '1',
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime DEFAULT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`estate_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
@@ -49,7 +51,7 @@ CREATE TABLE IF NOT EXISTS `estate_status` (
   `estate_status_id` int(11) NOT NULL AUTO_INCREMENT,
   `estate_status` varchar(50) NOT NULL DEFAULT '',
   `active` tinyint(2) NOT NULL DEFAULT '1',
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime DEFAULT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`estate_status_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
@@ -58,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `customer_type` (
   `customer_type_id` int(11) NOT NULL AUTO_INCREMENT,
   `customer_type` varchar(50) NOT NULL DEFAULT '',
   `active` tinyint(2) NOT NULL DEFAULT '1',
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime DEFAULT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`customer_type_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
@@ -71,7 +73,7 @@ CREATE TABLE IF NOT EXISTS `person_estate` (
   `owner` tinyint(2) NOT NULL DEFAULT '0',
   `executor` tinyint(2) NOT NULL DEFAULT '0',
   `active` tinyint(2) NOT NULL DEFAULT '1',
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime DEFAULT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`person_estate_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
@@ -79,14 +81,14 @@ CREATE TABLE IF NOT EXISTS `person_estate` (
 CREATE TABLE IF NOT EXISTS `item` (
   `item_id` int(11) NOT NULL AUTO_INCREMENT,
   `item_name` varchar(100) NOT NULL DEFAULT '',
-  `description` text NOT NULL DEFAULT '',
+  `description` text NOT NULL,
   `assigned_to_person_id` int(11) NOT NULL DEFAULT '0',
   `reserved_for_person_group_id` int(11) NOT NULL DEFAULT '0',
   `market_value` decimal(12, 2) NOT NULL DEFAULT 0, /* estimated value */
   `sell_price` decimal(12, 2) NOT NULL DEFAULT 0, /* actual price it was sold for */
   `min_bid` decimal(12, 2) NOT NULL DEFAULT 0,
   `active` tinyint(2) NOT NULL DEFAULT '1',
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime DEFAULT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`item_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
@@ -97,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `media` (
   `media_type_id` int(11) NOT NULL DEFAULT '0', /* 1=photo, 2=video, 3=audio */
   `file_name` varchar(100) NOT NULL DEFAULT '',
   `active` tinyint(2) NOT NULL DEFAULT '1',
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime DEFAULT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`media_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
@@ -106,7 +108,7 @@ CREATE TABLE IF NOT EXISTS `media_type` (
   `media_type_id` int(11) NOT NULL AUTO_INCREMENT,
   `media_type` varchar(20) NOT NULL DEFAULT '',
   `active` tinyint(2) NOT NULL DEFAULT '1',
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime DEFAULT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`media_type_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
@@ -118,9 +120,9 @@ CREATE TABLE IF NOT EXISTS `bid` (
   `bid` decimal(12, 2) NOT NULL DEFAULT 0,
   `max_bid` decimal(12, 2) NOT NULL DEFAULT 0,
   `active` tinyint(2) NOT NULL DEFAULT '1',
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime DEFAULT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`media_type_id`)
+  PRIMARY KEY (`bid_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
 
 /*
